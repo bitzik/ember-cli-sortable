4d45e8ffb4ec361b7ab65800cf2f09e31a00f1f8
leojpod <leo.jpod@gmail.com>
2015-04-10 09:54:50 +0000

    moved action triggered once the list has been readjusted by the user

--- addon/views/ember-sortable.js
+++ addon/views/ember-sortable.js
@@ -3,7 +3,8 @@ import Ember from 'ember';
 
 const {computed, EnumerableUtils, Logger, on} = Ember;
 
-export default Ember.CollectionView.extend({
+
+export default Ember.CollectionView.extend(Ember.TargetActionSupport, {
   /*
     private property, please don't mess with it...
    */
@@ -35,7 +36,47 @@ export default Ember.CollectionView.extend({
 
     content.removeAt(oldIndex);
     content.insertAt(newIndex, item);
-  }
+    this.sendAction('moved', content, oldIndex, newIndex);
+  },
 
 
+  /**
+   * Copied from `Ember.Component`. Read the `sendAction` documentation there
+   * for more information.
+   *
+   * @method sendAction
+   */
+  sendAction: function(action) {
+    var actionName;
+    var contexts = Array.prototype.slice.call(arguments, 1);
+
+    if (action === undefined) {
+      actionName = this.get('action');
+      Ember.assert('The default action was triggered on the component ' + this.toString() +
+        ', but the action name (' + actionName + ') was not a string.',
+        Ember.isNone(actionName) || typeof actionName === 'string');
+    } else {
+      actionName = this.get(action);
+      Ember.assert('The ' + action + ' action was triggered on the component ' +
+        this.toString() + ', but the action name (' + actionName +
+        ') was not a string.',
+        Ember.isNone(actionName) || typeof actionName === 'string');
+    }
+
+    // If no action name for that action could be found, just abort.
+    if (actionName === undefined) { return; }
+
+    this.triggerAction({
+      action: actionName,
+      actionContext: contexts
+    });
+  },
+
+  /**
+   * this is required to trigger Actions!
+   */
+  targetObject: computed('_parentView',function() {
+    var parentView = this.get('_parentView');
+    return parentView ? parentView.get('controller') : null;
+  })
 });
--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -1,7 +1,19 @@
 import Ember from 'ember';
 
+const {Logger} = Ember;
+
 export default Ember.Controller.extend({
   firstList: Ember.A([
     "this", "is", "just", "a", "simple", "list"
-  ])
+  ]),
+
+  actions: {
+    firstListMoved(item, oldIndex, newIndex) {
+      Logger.debug('item -> ', item);
+      Logger.debug('index: ', oldIndex, ' => ', newIndex);
+    },
+    moved() {
+      Logger.debug('moved !!!');
+    }
+  }
 });
